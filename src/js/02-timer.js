1999b42578ada57c2329ce1ff12e8a6d8ea6ed11
InnaShavarovska <innashavarovska@gmail.com>
2023-03-10 23:55:18 +0200

    + початок 2 завдання

---
+++ src/js/02-timer.js
@@ -0,0 +1,22 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import Notiflix from 'notiflix';
+
+const startBtn = document.querySelector('button[data-start]');
+const dateChosen = document.querySelector('#datetime-picker');
+const day = document.querySelector('[data-days]');
+const hours = document.querySelector('[data-hours]');
+const minutes = document.querySelector('[data-minutes]');
+const seconds = document.querySelector('[data-seconds]');
+
+const options = {
+	enableTime: true,
+	time_24hr: true,
+	defaultDate: new Date(),
+	minuteIncrement: 1,
+	onClose(selectedDates) {
+	  console.log(selectedDates[0]);
+	},
+ };
+
+ 
