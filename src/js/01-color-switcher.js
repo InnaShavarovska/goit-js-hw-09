b31a0569f26644655a799b969507e9e034dc3253
InnaShavarovska <innashavarovska@gmail.com>
2023-03-10 23:25:14 +0200

    +change 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
 
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+
+let timerId = null;
+
+function getRandomHexColor() {
+	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+ };
+
+const backgroundColorBody = function () {
+  body.style.backgroundColor = getRandomHexColor();
+};
+
+startBtn.addEventListener('click', () => {
+    timerId = setInterval(backgroundColorBody, 1000);
+	 startBtn.disabled = true;
+});
+
+stopBtn.addEventListener('click', ()=> {
+	clearInterval(timerId);
+	startBtn.disabled = false;
+});
+ 
+ 
