363f84de83c06f16c95559910ed100fffdc4ba58
InnaShavarovska <innashavarovska@gmail.com>
2023-03-11 19:50:48 +0200

    +change 2 task

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -16,7 +16,60 @@ const options = {
 	minuteIncrement: 1,
 	onClose(selectedDates) {
 	  console.log(selectedDates[0]);
-	},
+
+	  if(selectedDates[0] <=  new Date()) {
+		startBtn.disabled = true;
+		Notiflix.Notify.warning('Please choose a date in the future');
+	  } 
+	  if (selectedDates[0] > new Date()){
+		startBtn.disabled = false;
+		startBtn.addEventListener('click', timer);
+
+		function timer () {
+		intervalId = setInterval( () => {
+		let deltaTime = selectedDates[0] - new Date();
+      const timeComponents = convertMs(deltaTime);
+      updateClockFace(timeComponents);
+
+		if (deltaTime < 1000) {
+			clearInterval(intervalId);
+			startBtn.disabled = false;
+		 }
+		}
+	, 1000);
+		};
+	  }
+	}
  };
 
+flatpickr('#datetime-picker', options);
+
+function updateClockFace ({ days, hours, minutes, seconds }) {
+days.textContent = `${days}`;
+hours.textContent = `${hours}`;
+minutes.textContent = `${minutes}`;
+seconds.textContent = `${seconds}`;
+}
+
+ function convertMs(ms) {
+	// Number of milliseconds per unit of time
+	const second = 1000;
+	const minute = second * 60;
+	const hour = minute * 60;
+	const day = hour * 24;
  
+	// Remaining days
+	const days = addLeadingZero(Math.floor(ms / day));
+	// Remaining hours
+	const hours = addLeadingZero(Math.floor((ms % day) / hour));
+	// Remaining minutes
+	const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+	// Remaining seconds
+	const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+ 
+	return { days, hours, minutes, seconds };
+ };
+
+function addLeadingZero(value) {
+	return String(value).padStart(2, '0');
+};
